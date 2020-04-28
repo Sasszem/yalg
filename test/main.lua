a37e8f7514a2ef71054753e34de57fc1dfd59eca
Sasszem <barath.laci10@gmail.com>
2020-04-28 21:59:50 +0200

    update example

--- test/main.lua
+++ test/main.lua
@@ -5,14 +5,13 @@ local g = {}
 g[1] = GUI(
     HDiv(
         Label("A label"),
-        Button("Click me!", {font=Font(30), textColor=rgb(255, 0, 0), placement="center"})
+        Button("Click me!", {font=Font(40), textColor=rgb(255, 0, 0), placement="center", id="btnClickMe"})
     ),
     HDiv(
         Label("Another label"),
         Button("Do NOT click me!",{textColor=rgb(0,0,255), border=9, borderColor=rgb(0,255,0), backgroundColor=rgb(50,50,50)}),
         {
             font=Font(40),
-            textColor = rgb(0,255,0)
         }
     ),
     {
@@ -45,17 +44,20 @@ g[3] = GUI(
         Label("", {
             backgroundColor = rgb(255, 255, 255),
         })
-    ),
-    {
-        font = Font(20)
-    }
+    )
 )
+
+for k, v in pairs(g[1].widgets) do
+    print(k, v)
+end
 
 function love.load()
     love.window.setMode( 800, 600, {resizable=true, minwidth=100, minheight=100})
 end
 
 local sel = 1
+
+local t = 0
 
 function love.draw() 
     g[sel]:draw()
@@ -74,4 +76,15 @@ function love.resize(w, h)
     for i=1, #g do
         g[i]:resize(w, h)
     end
+end
+
+function love.update(dt)
+    t = t + dt
+    g[1].widgets.btnClickMe.text = "Time: "..tostring(math.floor(t))
+    if math.floor(t)%2 == 0 then
+        g[1].widgets.btnClickMe.style.borderColor = rgb(255, 0, 0)
+    else 
+        g[1].widgets.btnClickMe.style.borderColor = rgb(0, 255, 0)
+    end
+    g[1]:recalculate()
 end
