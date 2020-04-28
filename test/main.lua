c107ce4d3732dc59514cc1f732ee319b865abe35
Sasszem <barath.laci10@gmail.com>
2020-04-28 11:54:38 +0200

    Update examples

--- test/main.lua
+++ test/main.lua
@@ -20,14 +20,40 @@ g[1] = GUI(
     }
 )
 g[2] = GUI(
-    Button("New Game"),
-    Button("Highscores"),
-    Button("Quit"),
+    Button("New Game", {placement="fill"}),
+    Button("Highscores", {placement="fill"}),
+    Button("Quit", {placement="fill"}),
     {
         font=Font(30),
         placement="center"
     }
 )
+
+g[3] = GUI(
+    HDiv(
+        Label("", {
+            backgroundColor = rgb(255, 0, 0),
+        }),
+        Label("", {
+            backgroundColor = rgb(0, 255, 0),
+        })
+    ),
+    HDiv(
+        Label("", {
+            backgroundColor = rgb(0, 0, 255),
+        }),
+        Label("", {
+            backgroundColor = rgb(255, 255, 255),
+        })
+    ),
+    {
+        font = Font(20)
+    }
+)
+
+function love.load()
+    love.window.setMode( 800, 600, {resizable=true, minwidth=100, minheight=100})
+end
 
 local sel = 1
 
@@ -41,5 +67,11 @@ function love.keypressed(key, scancode, isrepeat)
         if sel>#g then
             sel = sel - #g
         end
+    end
+end
+
+function love.resize(w, h)
+    for i=1, #g do
+        g[i]:resize(w, h)
     end
 end
