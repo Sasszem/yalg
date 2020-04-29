881310830551d2a29acad30c258d4f1fd93b2b06
Sasszem <barath.laci10@gmail.com>
2020-04-29 21:15:44 +0200

    Update example

--- test/main.lua
+++ test/main.lua
@@ -5,11 +5,34 @@ local g = {}
 g[1] = GUI(
     HDiv(
         Label("A label"),
-        Button("Click me!", {font=Font(40), textColor=rgb(255, 0, 0), placement="center", id="btnClickMe"})
+        Button("Click me!", {
+            font=Font(40),
+            textColor=rgb(255, 0, 0), 
+            placement="center", 
+            id="btnClickMe"
+        })
     ),
     HDiv(
         Label("Another label"),
-        Button("Do NOT click me!",{textColor=rgb(0,0,255), border=9, borderColor=rgb(0,255,0), backgroundColor=rgb(50,50,50)}),
+        Button("Do NOT click me!",{
+            textColor=rgb(0,0,255), 
+            border=9, 
+            borderColor=rgb(0,255,0), 
+            backgroundColor=rgb(50,50,50), 
+            mouseEnter = function(self, x, y)
+                if self.text~="F U" then
+                    self.text = "DO NOT EVEN TRY!"
+                end
+            end,
+            mouseLeave = function (self, x, y)
+                if self.text~="F U" then
+                    self.text = "Do NOT click me!"
+                end
+            end,
+            click = function (self, x, y)
+                self.text = "F U"
+            end
+        }),
         {
             font=Font(40),
         }
@@ -40,16 +63,15 @@ g[3] = GUI(
     HDiv(
         Label("", {
             backgroundColor = rgb(0, 0, 255),
+            click = function (self, x, y, button)
+                self.style.backgroundColor[1] = 1 - self.style.backgroundColor[1]
+            end
         }),
         Label("", {
             backgroundColor = rgb(255, 255, 255),
         })
     )
 )
-
-for k, v in pairs(g[1].widgets) do
-    print(k, v)
-end
 
 function love.load()
     love.window.setMode( 800, 600, {resizable=true, minwidth=100, minheight=100})
@@ -79,6 +101,7 @@ function love.resize(w, h)
 end
 
 function love.update(dt)
+    g[sel]:update()
     t = t + dt
     g[1].widgets.btnClickMe.text = "Time: "..tostring(math.floor(t))
     if math.floor(t)%2 == 0 then
@@ -87,4 +110,8 @@ function love.update(dt)
         g[1].widgets.btnClickMe.style.borderColor = rgb(0, 255, 0)
     end
     g[1]:recalculate()
+end
+
+function love.mousepressed( x, y, button, istouch, presses )
+    g[sel]:handleClick(x, y, button)
 end
